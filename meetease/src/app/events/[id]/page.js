63ffd89f2e9b2aab9b993e43cc32d840df6a9420
kacperramenda <kacper.ramenda1@gmail.com>
2025-11-26 15:26:35 +0100

    feat: create event page template and cleaned unused variables

---
+++ meetease/src/app/events/[id]/page.js
@@ -0,0 +1,7 @@
+export default function eventPage() {
+    return (
+        <div>
+            <h1>Event Page</h1>
+        </div>
+    )
+}
--- meetease/src/utils/supabase/server.js
+++ meetease/src/utils/supabase/server.js
@@ -3,7 +3,6 @@ import { cookies } from 'next/headers'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export async function createClient() {
     if (!supabaseUrl || !supabaseKey) {
